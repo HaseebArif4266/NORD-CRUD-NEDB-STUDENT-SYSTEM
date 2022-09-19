a546dfb81939f508fabefe7010fb019c3a9f651e
haseeb arif <haseebarif9900@gmail.com>
2022-09-19 16:39:39 -0700

    app completed

--- students.js
+++ students.js
@@ -51,26 +51,43 @@ router.post('/', async(req,res) => {
     catch{
         res.status(200).json( {message: "Error in this API"})
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  })
 
+ router.patch('/:idVariable', async(req,res) => {
+    try{
+        await students.update({_id: req.params.idVariable}, req.body, {upsert: false}, (err, IsDataUpdated) => {
+            if(err){ return res.status(500).json( {message: "Error in the DB"}) };
+            if(IsDataUpdated){
+                res.status(200).json( { message: "student name: " + req.body.name + " updated successfully"})
+            }
+            else{
+                res.status(400).json( {message: "Student with this ID does not exist"})
+            }
+        })
+  
+    }
+    catch{
+        res.status(500).json( {message: "Error in this API"})
+    }
+})
 
 
+router.delete('/:idVariable', async(req,res) => {
+    try{
+        await students.remove({_id: req.params.idVariable}, (err, isDataDeleted) => {
+            if(err){ return res.status(500).json( {message: "Error in the DB"}) };
+            if(isDataDeleted){
+                res.status(200).json( { message: "student Deleted successfully"})
+            }
+            else{
+                res.status(400).json( {message: "Student with this ID does not exist"})
+            }
+        })
 
+    }
+    catch{
+        res.status(500).json( {message: "Error in this API"})
+    }
+})
 
 module.exports = router;
